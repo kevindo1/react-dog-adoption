b5364eb6f522eeca451d8f37d2c1529f449bd89f
Kevin Do <kevind0501@gmail.com>
2022-01-03 17:09:42 -0800

    added styling to the Dogs and Dog pages as well as added useParams

--- src/views/Dog/Dog.js
+++ src/views/Dog/Dog.js
@@ -2,19 +2,26 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import DogDetails from '../../components/DogDetails';
 import { fetchDogsById } from '../../services/dogs';
+import './Dog.css';
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
 
-export default function Dog(props) {
+export default function Dog() {
   const [dog, setDog] = useState([]);
-  const id = props.match.params.id;
+  const [loading, setLoading] = useState(true);
+  const params = useParams();
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await fetchDogsById(id);
+      const data = await fetchDogsById(params.id);
       setDog(data);
+      setLoading(false);
     };
     fetchData();
-  }, [id]);
+  }, [params.id]);
 
+  if (loading) {
+    return <h3>Loading</h3>;
+  }
   return (
     <>
       <div>
--- src/views/Dogs/Dogs.js
+++ src/views/Dogs/Dogs.js
@@ -6,14 +6,20 @@ import './Dogs.css';
 
 export default function Dogs() {
   const [dogs, setDogs] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchDogs();
       setDogs(data);
+      setLoading(false);
     };
     fetchData();
   }, []);
+
+  if (loading) {
+    return <h3>Loading</h3>;
+  }
 
   return (
     <div>
